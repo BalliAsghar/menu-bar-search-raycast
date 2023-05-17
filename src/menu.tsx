90ed9862aca86a65a09829487f36bc436513488c
Balli Asghar <ballihacker@outlook.com>
2023-05-17 23:09:19 +0100

    using memo

--- src/menu.tsx
+++ src/menu.tsx
@@ -1,10 +1,13 @@
-import { Action, ActionPanel, Icon, List, getFrontmostApplication } from "@raycast/api";
+import { Action, ActionPanel, List, getFrontmostApplication } from "@raycast/api";
 import { useMenuBar } from "./useCachedMenuBar";
 import { MenuBar } from "./types";
 import { clickMenuBarOption, Settings } from "./utils";
 import { useEffect, useState } from "react";
+import React from "react";
 
-export default function Menu() {
+interface MenuProps {}
+Settings("showKeyboardShortcuts");
+function Menu(_props: MenuProps) {
   const [app, setApp] = useState<string>("");
   const menuBar = useMenuBar(app);
 
@@ -18,25 +21,28 @@ export default function Menu() {
 
   return (
     <List isLoading={!app} searchBarPlaceholder="Filter menu items by name..." navigationTitle={app}>
-      {menuBar.map((menu: MenuBar) => (
-        <List.Item
-          title={menu.title}
-          subtitle={menu.subtitle}
-          key={menu.arg}
-          icon={{ fileIcon: menu.icon }}
-          actions={
-            <ActionPanel>
-              <Action title="Click" onAction={async () => await clickMenuBarOption(menu.arg)} />
-            </ActionPanel>
-          }
-          {...(Settings("showKeyboardShortcuts") && {
-            ...(menu.shortcut && {
-              accessoryIcon: Icon.Keyboard,
-              accessoryTitle: menu.shortcut,
-            }),
-          })}
-        ></List.Item>
-      ))}
+      {menuBar.map((menu: MenuBar) => {
+        const { title, subtitle, icon, shortcut, arg } = menu;
+
+        return (
+          <List.Item
+            title={title}
+            subtitle={subtitle}
+            key={arg}
+            icon={{ fileIcon: icon }}
+            accessories={
+              Settings("showKeyboardShortcuts") ? [{ text: shortcut, tooltip: "Keyboard Shortcut" }] : undefined
+            }
+            actions={
+              <ActionPanel>
+                <Action title="Click" onAction={async () => await clickMenuBarOption(arg)} />
+              </ActionPanel>
+            }
+          />
+        );
+      })}
     </List>
   );
 }
+
+export default React.memo(Menu);
